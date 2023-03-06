9e292914a9ad84eb79553d7335461a4c1ed81136
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-03-07 01:15:24 +0530

    Add files via upload

--- src/engine.js
+++ src/engine.js
@@ -288,8 +288,8 @@ async function searchWeb(query) {
     const link = firstResult.link;
     const  description = firstResult.snippet;
     
-    //return `${title} - ${description} - ${link}`;
-    return `${description}`;
+    return `${title} - ${description} - ${link}`;
+    //return `${description}`;
   } else {
     return "Sorry, I could not find any results.";
   }
