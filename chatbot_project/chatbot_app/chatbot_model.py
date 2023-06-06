1224e84325e958fd1501a898afbdb3e95d431eac
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-07 01:08:36 +0530

    commit

--- chatbot_project/chatbot_app/chatbot_model.py
+++ chatbot_project/chatbot_app/chatbot_model.py
@@ -1,23 +1,35 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 import joblib
+
 
 class ChatbotModel:
     def __init__(self):
-        self.pipeline = Pipeline([
-            ('tfidf', TfidfVectorizer()),
-            ('clf', LogisticRegression())
-        ])
+        self.vectorizer = TfidfVectorizer()
+        self.X_train_tfidf = None
+        self.y_train = None
     
     def train(self, X, y):
-        self.pipeline.fit(X, y)
+        self.y_train = y
+        self.X_train_tfidf = self.vectorizer.fit_transform(X)
     
     def predict(self, X):
-        return self.pipeline.predict(X)
+        X_tfidf = self.vectorizer.transform(X)
+        scores = cosine_similarity(X_tfidf, self.X_train_tfidf)
+        predicted_indices = np.argmax(scores, axis=1)
+        predicted_responses = [self.y_train[idx] for idx in predicted_indices]
+        return predicted_responses
     
-    def load_model(self, model_path):
-        self.pipeline = joblib.load(model_path)
+    def save_model(self, model_path):
+        joblib.dump(self, model_path)
+    
+    def load_model(self,model_path):
+        loaded_model = joblib.load(model_path)
+        self.vectorizer = loaded_model.vectorizer
+        self.X_train_tfidf = loaded_model.X_train_tfidf
+        self.y_train = loaded_model.y_train
+
 
 chatbot = ChatbotModel()
 
@@ -33,14 +45,14 @@ X_train = [
 
 y_train = [
     "Hey",
-    "As an AI , I don't have any specific name , but you can call me JERN , Just an Experimental response network",
-    "I was created by vivek thakur",
-    "I am an AI , so i don't have any specific owner",
+    "As an AI, I don't have any specific name, but you can call me JERN, Just an Experimental response network",
+    "I was created by Vivek Thakur",
+    "I am an AI, so I don't have any specific owner",
     "What things would you like me to do?",
     "I'm good, and you?",
-    "Take Care , Hare Krishna",
+    "Take care, Hare Krishna",
 ]
 
 chatbot.train(X_train, y_train)
 model_path = './model.pkl'
-joblib.dump(chatbot, model_path)
+chatbot.save_model(model_path)
