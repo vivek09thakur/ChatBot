e0684a0dec364bf299f03d40b9ec99d093c7ac5f
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-07 06:15:58 +0530

    commit

--- chatbot_project/chatbot_app/views.py
+++ chatbot_project/chatbot_app/views.py
@@ -5,18 +5,20 @@ from django.shortcuts import render
 from .chatbot_model import ChatbotModel
 
 def chatbot_view(request):
+    
     if request.method == 'POST':
         message = request.POST['message']
-        
-        # Create an instance of the chatbot model
         chatbot = ChatbotModel()
-        
-        # Load the trained model (you can implement this method)
         chatbot.load_model('./model.pkl')
         
         # Get the prediction from the chatbot model
         response = chatbot.predict([message])
+        history = request.session.get('history', [])
+        history.append({'message': message, 'response': response})
+        request.session['history'] = history
         
-        return render(request, 'chatbot_app/chatbot.html', {'response': response})
+        return render(request, 'chatbot_app/chatbot.html', {'history': history})
+        
+        # return render(request, 'chatbot_app/chatbot.html', {'response': response})
     
     return render(request, 'chatbot_app/chatbot.html')
