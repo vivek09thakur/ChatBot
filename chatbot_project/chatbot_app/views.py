e8573c9c8d9e53335cd52475f6d61b05e37cc7e8
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-07 06:52:48 +0530

    commit

--- chatbot_project/chatbot_app/views.py
+++ chatbot_project/chatbot_app/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from .chatbot_model import ChatbotModel
 
 def chatbot_view(request):
-    
     if request.method == 'POST':
-        message = request.POST['message']
-        chatbot = ChatbotModel()
-        chatbot.load_model('./model.pkl')
-        
-        # Get the prediction from the chatbot model
-        response = chatbot.predict([message])
-        history = request.session.get('history', [])
-        history.append({'message': message, 'response': response})
-        request.session['history'] = history
-        
-        return render(request, 'chatbot_app/chatbot.html', {'history': history})
-        
-        # return render(request, 'chatbot_app/chatbot.html', {'response': response})
-    
+        message = request.POST.get('message', '').strip()
+
+        if message:
+            chatbot = ChatbotModel()
+            chatbot.load_model('./model.pkl')
+
+            # Get the prediction from the chatbot model
+            response = chatbot.predict([message])
+            history = request.session.get('history', [])
+            history.append({'message': message, 'response': response})
+            request.session['history'] = history
+        else:
+            # If no message is provided, return the current history
+            history = request.session.get('history', [])
+
+        return render(request, 'chatbot_app/chatbot.html', {'history': history, 'response': response})
+
     return render(request, 'chatbot_app/chatbot.html')
