3700846c070162537b9879c1ed1cedd4faf96ad7
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-08 13:22:00 +0530

    commit

--- chatbot_project/chatbot_project/settings.py
+++ chatbot_project/chatbot_project/settings.py
@@ -128,4 +128,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = os.path.join(BASE_DIR,"static")
+
 STATIC_ROOT = os.path.join(BASE_DIR,"vercel_build","static")
