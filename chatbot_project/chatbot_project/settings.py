71f45b9485de8c8a6c223f64399c29c2bf9617fd
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-08 13:12:46 +0530

    commit

--- chatbot_project/chatbot_project/settings.py
+++ chatbot_project/chatbot_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-r)z4v232bp+lmes6q2m5agj-0j_ac%6ja52ti=u^kg)ok&faph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
--- vercel_build.sh
+++ vercel_build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py runserver 0.0.0.0:$PORT
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate
+# python manage.py runserver 0.0.0.0:$PORT
+
+echo "BUILD START"
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
+echo "BUILD END"
+
+
