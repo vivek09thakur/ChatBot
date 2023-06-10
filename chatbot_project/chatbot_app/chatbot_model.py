ffe99a5ef4e2db1c2c152fa20bd7e6b6b5bd70c4
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-11 01:12:42 +0530

    commit

--- chatbot_project/chatbot_app/chatbot_model.py
+++ chatbot_project/chatbot_app/chatbot_model.py
@@ -2,6 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import joblib
+import json
 
 
 class ChatbotModel:
@@ -33,40 +34,11 @@ class ChatbotModel:
 
 chatbot = ChatbotModel()
 
-X_train = [
-    "Hello",
-    "What is your name?",
-    "Who created you?",
-    "Who owns you?",
-    "What things you can do?",
-    "How are you?",
-    "Bye",
-    "What is the meaning of life?",
-    "Can you tell me a joke?",
-    "What's the weather like today?",
-    "How does the stock market work?",
-    "What is the capital of France?",
-    "How do I bake a chocolate cake?",
-    "Tell me about the latest technology trends.",
-]
+with open('./TrainingData/training_data.json','r') as f:
+    data = json.load(f)
 
-y_train = [
-    "Hey",
-    "As an AI, I don't have any specific name, but you can call me JERN, Just an Experimental response network",
-    "I was created by Vivek Thakur",
-    "I am an AI, so I don't have any specific owner",
-    "What things would you like me to do?",
-    "I'm good, and you?",
-    "Take care, Hare Krishna",
-    "The meaning of life can vary for each person, but it generally revolves around finding purpose and happiness.",
-    "Sure! Here's a joke for you: Why don't scientists trust atoms? Because they make up everything!",
-    "I'm sorry, I don't have access to real-time weather information. You can check a weather website or app for the latest updates.",
-    "The stock market is a place where buyers and sellers trade shares of publicly traded companies. Prices are determined by supply and demand.",
-    "The capital of France is Paris.",
-    "To bake a chocolate cake, you'll need ingredients like flour, sugar, cocoa powder, eggs, and butter. There are many recipes available online that you can follow.",
-    "The latest technology trends include artificial intelligence, blockchain, virtual reality, and Internet of Things (IoT). These fields are rapidly evolving and shaping various industries.",
-
-]
+X_train = data['input']
+y_train = data['responses']
 
 chatbot.train(X_train, y_train)
 model_path = './model.pkl'
