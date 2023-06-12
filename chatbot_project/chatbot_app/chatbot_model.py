f00fbe19902598cc12ee343985118f9d95d28c80
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-12 15:09:23 +0530

    COMMIT

--- chatbot_project/chatbot_app/chatbot_model.py
+++ chatbot_project/chatbot_app/chatbot_model.py
@@ -11,7 +11,11 @@ class ChatbotModel:
         self.vectorizer = TfidfVectorizer()
         self.X_train_tfidf = None
         self.y_train = None
-    
+        self.offensive_response = ['I am sorry but I am not designed to any kind of offensive prompt , that may contain any inappropriate contexr.']
+        self.offensive_words = [
+            'fuck','sex','dick','suck my dick','asshole','motherfucker','coward','pivort','porn','pussy','prostitue','chutiya','gandu',
+            'madarchod','behenchod','betichod','dhotarchod','bhosadike','gaand','gand','chut','lund','bc','mc','bsdk','gaand marao','chuso'
+        ]
     def train(self, X, y):
         self.y_train = y
         self.X_train_tfidf = self.vectorizer.fit_transform(X)
@@ -21,6 +25,11 @@ class ChatbotModel:
         scores = cosine_similarity(X_tfidf, self.X_train_tfidf)
         predicted_indices = np.argmax(scores, axis=1)
         predicted_responses = [self.y_train[idx] for idx in predicted_indices]
+
+        for words in self.offensive_response:
+            if words in X[0].lower():
+                return (self.offensive_response)
+            
         return predicted_responses
     
     def save_model(self, model_path):
--- chatbot_project/chatbot_project/settings.py
+++ chatbot_project/chatbot_project/settings.py
@@ -82,7 +82,7 @@ STATICFILES_DIRS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'JERN_CHATDATA.db',
     }
 }
 
