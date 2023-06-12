027bb4a007a350781d38791a328f9f8146ac0af4
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-13 02:15:31 +0530

    commit

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -4,17 +4,15 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
 
+# Load the training data from the JSON file
+with open('data/training_data.json') as f:
+    data = json.load(f)
+
+training_data = data['input']
+responses = data['responses']
+default_response = data['default']
 
 def chatbot_view(request):
-
-    # Load the training data from the JSON file
-    with open('data/training_data.json') as f:
-        data = json.load(f)
-
-    training_data = data['input']
-    responses = data['responses']
-    default_response = data['default']
-
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(training_data)
 
@@ -23,21 +21,14 @@ def chatbot_view(request):
 
         if message:
             message_bow = vectorizer.transform([message])
+            response_idx = cosine_similarity(message_bow, X).argmax()
+            similarity_score = cosine_similarity(message_bow, X).max()
 
-            # Update the X matrix with the new message
-            X = np.vstack([X.A, message_bow.A])
+            if response_idx < len(responses):
 
-            similarity_scores = cosine_similarity(message_bow, X)
-            response_idx = similarity_scores.argmax()
-            similarity_score = similarity_scores.max()
-
-            if similarity_score < 0.75:
-
-                response = default_response
-
-                if response == default_response:
-                    # Check if training_data is not empty
-                    prompt = training_data[-1] if training_data else "" # Get the last input as the prompt
+                if similarity_score < 0.75:
+                    response = default_response
+                    prompt = training_data[-1]  # Get the last input as the prompt
 
                     # Append the prompt to the input list
                     training_data.append(prompt)
@@ -53,20 +44,20 @@ def chatbot_view(request):
                     with open('data/training_data.json', 'w') as f:
                         json.dump(data, f)
 
-                    X = vectorizer.fit_transform(training_data)
+                else:
+                    response = responses[response_idx]
 
+                    # Append the message as the prompt for the next input
+                    training_data.append(message)
+
+                    # Update the data dictionary
+                    data['input'] = training_data
+
+                    # Update the JSON file with the new data
+                    with open('data/training_data.json', 'w') as f:
+                        json.dump(data, f)
             else:
-                response = responses[response_idx]
-
-                # Append the message as the prompt for the next input
-                training_data.append(message)
-
-                # Update the data dictionary
-                data['input'] = training_data
-
-                # Update the JSON file with the new data
-                with open('data/training_data.json', 'w') as f:
-                    json.dump(data, f)
+                response = default_response
 
             history = request.session.get('history', [])
             history.append({'message': message, 'response': response})
