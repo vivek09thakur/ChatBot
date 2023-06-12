00a46d9894477962a1918ddfb300354b80baa208
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-12 16:24:46 +0530

    commit

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -79,12 +79,12 @@ STATICFILES_DIRS = [
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'JERN_CHATDATA.db',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'JERN_CHATDATA.db',
+    }
+}
 
 
 # Password validation
