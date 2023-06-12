7a2a299c1cbcab62cf98830e8d6ede590c8d44e9
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-13 03:35:50 +0530

    commit

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -1,82 +1,21 @@
-import numpy as np
 from django.shortcuts import render, redirect
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import json
-
+from .chatbot_model import ChatbotModel
 
 def chatbot_view(request):
-    # Load the training data from the JSON file
-    with open('data/training_data.json') as f:
-        data = json.load(f)
-
-    training_data = data['input']
-    responses = data['responses']
-    default_response = data['default']
-
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(training_data)
-
     if request.method == 'POST':
         message = request.POST.get('message', '').strip()
 
-        if  message == 'clear' or  message == 'clear chat' or  message == 'clear history':
-            
-            # Clear the history by setting an empty list
-            request.session['history'] = []
-
         if message:
-            message_bow = vectorizer.transform([message])
-
-            # Update the X matrix with the new message
-            X = np.vstack([X.A, message_bow.A])
-
-            similarity_scores = cosine_similarity(message_bow, X)
-            response_idx = similarity_scores.argmax()
-            similarity_score = similarity_scores.max()
-
-            if similarity_score < 0.75 :
-                response = default_response[0]
-
-                #  Check if training_data has at least one element
-                prompt = training_data[-1]  if training_data else '' # Get the last input as the prompt
-                
-
-                # Append the prompt to the input list
-                training_data.append(prompt)
-
-                # Append the message as the response
-                responses.append(message)
-
-                # Update the data dictionary
-                data['input'] = training_data
-                data['responses'] = responses
-
-                # Update the JSON file with the new data
-                with open('data/training_data.json', 'w') as f:
-                    json.dump(data, f)
-
-            if response_idx < len(responses):
-                response = responses[response_idx]
-            
-            else:
-                response = default_response[0]
-
-                training_data.append(message)
-
-                # Update the data dictionary
-                data['input'] = training_data
-
-                # Update the JSON file with the new data
-                with open('data/training_data.json', 'w') as f:
-                    json.dump(data, f)
-            request.session['history'] = []
+            chatbot = ChatbotModel()
+            response = chatbot.generate_response(message)
             history = request.session.get('history', [])
             history.append({'message': message, 'response': response})
             request.session['history'] = history
 
+            # Redirect to a new URL after processing the form
             return redirect('index')
 
+    # If the request method is not POST, return the current history without generating a response
     history = request.session.get('history', [])
     response = None
 
