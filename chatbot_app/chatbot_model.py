e8ea2b82c54091cf177bbcee5bdac9a794e6825e
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-13 15:25:06 +0530

    commit

--- chatbot_app/chatbot_model.py
+++ chatbot_app/chatbot_model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from keras.utils.data_utils import pad_sequences
 import json
+import os
 
 
 class ChatbotModel:
@@ -15,18 +16,20 @@ class ChatbotModel:
         self.model = None
         self.load_or_train_model()
 
+
     def load_or_train_model(self):
-        try:
-            self.model = tf.keras.models.load_model('./saved_model/vs2306.h5')
+        model_file = './saved_model/vs2306.h5'
+        if os.path.exists(model_file):
+            self.model = tf.keras.models.load_model(model_file)
             with open('./database/chat_log.json', 'r') as f:
                 data = json.load(f)
                 self.word_to_int = data['word_to_int']
                 self.padded_data = np.array(data['padded_data'])
                 self.label_to_int = data['label_to_int']
                 self.responses = data['responses']
-        except (OSError, IOError):
+        else:
             self.train_model()
-            self.model.save('./saved_model/vs23-08.h5')
+            self.model.save(model_file)
             data = {
                 'word_to_int': self.word_to_int,
                 'padded_data': self.padded_data.tolist(),
@@ -35,7 +38,7 @@ class ChatbotModel:
             }
             with open('./database/chat_log.json', 'w') as f:
                 json.dump(data, f)
-
+    
     def train_model(self):
         with open(self.training_data_file, 'r') as f:
             training_data = json.load(f)
@@ -63,12 +66,12 @@ class ChatbotModel:
         self.model = tf.keras.Sequential([
             tf.keras.layers.Embedding(len(unique_words), 512, input_length=max_length),
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(32, activation="relu"),
+            tf.keras.layers.Dense(200, activation="relu"),
             tf.keras.layers.Dense(len(set(labels)), activation="softmax")
         ])
 
         self.model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-        self.model.fit(np.array(self.padded_data), np.array(int_encoded_labels),epochs=200)
+        self.model.fit(np.array(self.padded_data), np.array(int_encoded_labels),epochs=5000)
 
     def generate_response(self, text):
         int_encoded_text = [self.word_to_int[word] for word in text.split() if word in self.word_to_int]
@@ -78,5 +81,6 @@ class ChatbotModel:
         for label, int_label in self.label_to_int.items():
             if int_label == max_prob_index:
                 return self.responses[int_label]
-            
-# bot = ChatbotModel('./database/intents.json')
+
+
+# bot = ChatbotModel('./database/intents.json')  # Comment out or remove this line
