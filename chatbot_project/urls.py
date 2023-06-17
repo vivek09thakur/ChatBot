97409f33661b5bf0331c7acf440cb7535e23702f
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-17 17:09:34 +0530

    commit

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', chatbot_view, name='index'),
     path('', include('chatbot_app.urls')),
 ]
 
