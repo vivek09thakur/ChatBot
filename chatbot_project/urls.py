1a97e85d8d5348db679118de86a0a5fb6430cc46
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-19 00:41:59 +0530

    commit

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
-from chatbot_app.views import chatbot_view
+from chatbot_app import views
 from django.conf import settings
 from django.conf.urls.static import static
 
