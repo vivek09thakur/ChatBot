191fa5c03a79b5b9e64cd7ab9900d17dc8067a3a
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-18 13:38:11 +0530

    commit

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -8,19 +8,17 @@ def chatbot_view(request):
         if message:
             chatbot = ChatbotModel('./database/intents.json')
             response = chatbot.generate_response(message)
+
             history = request.session.get('history', [])
             history.append({'message': message, 'response': response})
             request.session['history'] = history
 
-            # Redirect to a new URL after processing the form
             return redirect('index')
-        
+
         if 'delete_history' in request.POST:
-        # Delete the history if the delete button is clicked
             del request.session['history']
             return redirect('index')
 
-    # If the request method is not POST, return the current history without generating a response
     history = request.session.get('history', [])
     response = None
 
