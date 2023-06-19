3cdfa7bcfac7f0a470b3b68f98e92781fab61598
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-20 01:10:58 +0530

    commit

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -2,25 +2,29 @@ from django.shortcuts import render, redirect
 from .chatbot_model import ChatbotModel
 
 def chatbot_view(request):
-    if request.method == 'POST':
-        message = request.POST.get('message', '').strip()
 
-        if message:
-            chatbot = ChatbotModel('./database/intents.json')
-            response = chatbot.generate_response(message)
-            history = request.session.get('history', [])
-            history.append({'message': message, 'response': response})
-            request.session['history'] = history
+    try :
+        if request.method == 'POST':
+            message = request.POST.get('message', '').strip()
 
-            # Redirect to a new URL after processing the form
-            return redirect('home')
-        
-        if 'delete_history' in request.POST:
-        # Delete the history if the delete button is clicked
-            del request.session['history']
-            return redirect('home')
+            if message:
+                chatbot = ChatbotModel('./database/intents.json')
+                response = chatbot.generate_response(message)
+                history = request.session.get('history', [])
+                history.append({'message': message, 'response': response})
+                request.session['history'] = history
 
-    # If the request method is not POST, return the current history without generating a response
-    history = request.session.get('history', [])
-    response = None
-    return render(request, 'chatbot_app/index.html', {'history': history, 'response': response})
+                # Redirect to a new URL after processing the form
+                return redirect('home')
+
+            if 'delete_history' in request.POST:
+            # Delete the history if the delete button is clicked
+                del request.session['history']
+                return redirect('home')
+
+        # If the request method is not POST, return the current history without generating a response
+        history = request.session.get('history', [])
+        response = None
+        return render(request, 'chatbot_app/index.html', {'history': history, 'response': response})
+    except Exception:
+        return redirect('home')
