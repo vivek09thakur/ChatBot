1cd172cc8d4b993677fefe7245c59630c60c93c5
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-06-23 00:13:41 +0530

    commit

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 import json
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.naive_bayes import MultinomialNB
 
 with open('./database/intents.json') as f:
     data = json.load(f)
@@ -12,20 +12,10 @@ responses = data['responses']
 
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(training_data)
-y = list(range(len(training_data)))
+y = np.array(range(len(training_data)))  # Convert y to NumPy array
 
-def get_best_response(message):
-    message_bow = vectorizer.transform([message])
-    similarity_scores = cosine_similarity(message_bow, X)
-    best_idx = np.argmax(similarity_scores)
-    best_score = similarity_scores[0][best_idx]
-
-    if best_score >= 0.5:
-        response = responses[best_idx]
-    else:
-        response = None
-
-    return response
+clf = MultinomialNB()
+clf.fit(X, y)
 
 def add_new_response(message, new_response):
     data['prompts'].append(message)
@@ -37,19 +27,30 @@ def chatbot_view(request):
     try:
         if request.method == 'POST':
             message = request.POST.get('message', '').strip()
+            new_response = request.POST.get('new_response', '').strip()
 
             if message:
-                response = get_best_response(message)
+                message_bow = vectorizer.transform([message])
+                confidence_scores = clf.predict_proba(message_bow)
+                max_confidence = np.max(confidence_scores)
+                response_idx = clf.predict(message_bow)[0]
 
-                if response is None:
+                if max_confidence >= 0.5:
+                    response = responses[response_idx]
+                else:
                     response = 'Sorry, but I am not able to respond to this kind of prompt. If you would like, you can teach me how to respond to it.'
-                    new_response = request.POST.get('new_response', '').strip()
+
                     if new_response:
-                        add_new_response(message=message,new_response=new_response)
-                        thnx_msg = 'Thanks for teaching me something new!'
+                        add_new_response(message, new_response)
+                        training_data.append(message)
+                        responses.append(new_response)
+                        X = vectorizer.fit_transform(training_data)
+                        y = np.array(range(len(training_data)))
+                        clf.fit(X, y)
+                        response = 'Thanks for teaching me something new!'
 
                 history = request.session.get('history', [])
-                history.append({'message': message, 'response': response,'thnx_msg':thnx_msg})
+                history.append({'message': message, 'response': response})
                 request.session['history'] = history
 
                 return redirect('home')
@@ -60,7 +61,7 @@ def chatbot_view(request):
 
         history = request.session.get('history', [])
         response = None
-        return render(request, 'chatbot_app/index.html', {'history': history, 'response': response,'thnx_msg':thnx_msg})
+        return render(request, 'chatbot_app/index.html', {'history': history, 'response': response})
 
     except Exception:
         return redirect('home')
