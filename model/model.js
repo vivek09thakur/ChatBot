e15dc29c114158411efd50ce933838be2dba37b6
Vivek Thakur <20509.vivek@gpranchi.org>
2023-10-04 17:21:25 +0530

    commit

--- model/model.js
+++ model/model.js
@@ -6,18 +6,42 @@ class ChatBot {
     this.chatInput = document.getElementById("chat-input");
     this.sendButton = document.getElementById("send");
 
-    this.sendButton.addEventListener("click", this.handleUserInput);
+    this.sendButton.addEventListener("click", () => this.handleUserInput());
+    this.chatInput.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        this.handleUserInput();
+      }
+    });
+
     this.responses = Response;
+    this.typingDelay = 5;
   }
 
-  handleUserInput = () => {
+  async simulateTyping(message) {
+    const botMessage = this.createMessage('', 'bot');
+    this.chatLog.appendChild(botMessage);
+
+    for (let i = 0; i < message.length; i++) {
+      botMessage.textContent += message[i];
+      await this.wait(this.typingDelay);
+    }
+
+    this.scrollToBottom();
+  }
+
+  async handleUserInput() {
     const userMessage = this.chatInput.value;
-    if (userMessage === "") return;
+    if (userMessage === '') return;
     this.appendUserMessage(userMessage);
     const response = this.generateResponse(userMessage);
-    this.appendBotMessage(response);
-    this.chatInput.value = "";
-  };
+
+    await this.simulateTyping(response);
+    this.chatInput.value = '';
+  }
+
+  async wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   appendBotMessage(message) {
     const botMessage = this.createMessage(message, "bot");
